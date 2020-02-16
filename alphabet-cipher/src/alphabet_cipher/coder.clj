8141e5f9cff7d877ba03d060de18863e3159fdac
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2020-02-16 19:14:31 +0100

    Solving alphabet_cider/coder encode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,24 @@
 (ns alphabet-cipher.coder)
 
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
+
+(def alphabet (char-range \a \z))
+
+(def a-index (int \a))
+
+(defn alphabet-index [char]
+  (- (int char) a-index))
+
+(defn rotate [row col]
+ (let [row-index (alphabet-index row)
+       col-index (alphabet-index col)]
+   (nth (cycle alphabet) (+ col-index row-index))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [keywords (cycle keyword)
+        chars (map #(rotate %1 %2) message keywords)]
+    (apply str chars)))
 
 (defn decode [keyword message]
   "decodeme")
